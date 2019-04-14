03e7b94b9d76badf153b66a30729017aa20c7357
Biswajit Behera <suraj53028@gmail.com>
2019-04-14 12:44:06 +0530

    implement search

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shoppy/ui/home_page.dart';
 
-void main() => runApp(new MyApp());
+void main() async{
+  await Firestore.instance.settings(timestampsInSnapshotsEnabled: true);
+  runApp(new MyApp());}
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
--- lib/ui/catalog.dart
+++ lib/ui/catalog.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:shoppy/model/product.dart';
+
 class CataLog extends StatefulWidget {
   @override
   _CataLogState createState() => _CataLogState();
@@ -16,49 +17,107 @@ class _CataLogState extends State<CataLog> {
       appBar: AppBar(
         title: Text("CateLog"),
       ),
-      body: 
-      // Center(
-      //   child: productData.isEmpty
-      //       ? Column(
-      //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //         children: <Widget>[
-      //           Text("Select csv file"),
-      //           RaisedButton(
-      //             onPressed: ()async{
-                  
-      //             },
-      //             child: Text("SELECT"),
-                  
-      //           ),
-      //           Text(path??"Select plese")
-      //         ],
-      //       )
-      //       : 
-            FutureBuilder(
-              future: DefaultAssetBundle.of(context).loadString("lib/data/data.json"),
-              builder: (context,snapshot){
-                var jsonResponse=json.decode(snapshot.data.toString());
-                List<ProductData> mydata=ProductDataList.fromJSOn(jsonResponse).list;
-                return GridView.count(
-                crossAxisCount: 2,
-                children: mydata
-                    .map((p) => Card(
-                          child: Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Text(p.name),
-                                Text(p.price.toString())
-                              ],
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: () {
+          showSearch(
+            context: context,
+            delegate: MyDelegate(productData),
+          );
+        },
+      ),
+      body: FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString("lib/data/data.json"),
+        builder: (context, snapshot) {
+          var jsonResponse = json.decode(snapshot.data.toString());
+          productData = ProductDataList.fromJSOn(jsonResponse).list;
+
+          return productData.isEmpty
+              ? Center(
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text("Select csv file"),
+                    RaisedButton(
+                      onPressed: () async {},
+                      child: Text("SELECT"),
+                    ),
+                    Text(path ?? "Select plese")
+                  ],
+                ))
+              : GridView.count(
+                  crossAxisCount: 2,
+                  children: productData
+                      .map((p) => Card(
+                            child: Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  Text(p.name),
+                                  Text(p.price.toString())
+                                ],
+                              ),
                             ),
-                          ),
-                        ))
-                    .toList(),
-              );
-              },
-            )
-      
+                          ))
+                      .toList(),
+                );
+        },
+      ),
+    );
+  }
+}
+
+class MyDelegate extends SearchDelegate {
+  final List<ProductData> productData;
+
+  MyDelegate(this.productData);
+  
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [IconButton(icon: Icon(Icons.close), onPressed: () => query = "")];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: this.transitionAnimation,
+      ),
+      onPressed: () => Navigator.pop(context),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center(
+      child: Card(
+        color: Colors.green,
+        child: Container(
+              height: 300,
+              width: 300,
+              child: Text(query),
+      ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    var suggestionList=query.isEmpty?productData:productData.where((p)=>p.name.contains(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(suggestionList[index].name),
+          onTap: (){
+            showResults(context);
+          },
+        );
+      },
+      itemCount: suggestionList.length,
     );
   }
 }
